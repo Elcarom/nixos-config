77168df9d6fe0bc129d242d3b036fe5e2a420b2c
Elcarom <elcarom@proton.me>
2026-05-18 22:50:16 +0200

    Added keyboard layout

--- modules/nixos/core/locale.nix
+++ modules/nixos/core/locale.nix
@@ -6,4 +6,12 @@
   time.timeZone = "Europe/Paris";
 
   i18n.defaultLocale = "en_GB.UTF-8";
+
+  console.useXkbConfig = true;
+
+  services.xserver.xkb = {
+    layout = "fr";
+    variant = "";
+  };
+
 }
