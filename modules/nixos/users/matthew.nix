33ae2e59e90844288334422be3a50aa88fe35baa
Elcarom <elcarom@proton.me>
2026-05-18 23:18:43 +0200

    Added hashed password

--- modules/nixos/users/matthew.nix
+++ modules/nixos/users/matthew.nix
@@ -6,6 +6,7 @@
 {
   users.users.matthew = {
     isNormalUser = true;
+    hashedPassword = "$6$cKRN/IfoXGs4.xe6$dm7RVQRyIt5TlpRBFK4geFar8Kvk2aUUxjsNNPedAXT0Tp1KpXniBpK12CfvZHTR.03uYgL8fjqPdR6K0VmAH1";
 
     extraGroups = [
       "wheel"
