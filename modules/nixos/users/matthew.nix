68db14298fd6b4fcf4bbd8543dd25282b4a26ec5
Elcarom <elcarom@proton.me>
2026-05-19 19:53:06 +0200

    Update hashed password for user 'matthew'

--- modules/nixos/users/matthew.nix
+++ modules/nixos/users/matthew.nix
@@ -6,7 +6,7 @@
 {
   users.users.matthew = {
     isNormalUser = true;
-    hashedPassword = "$6$cKRN/IfoXGs4.xe6$dm7RVQRyIt5TlpRBFK4geFar8Kvk2aUUxjsNNPedAXT0Tp1KpXniBpK12CfvZHTR.03uYgL8fjqPdR6K0VmAH1";
+    hashedPassword = "$6$mgao3Qxd9W5eIa0R$KQO8oLFDjTViaTv2BDDaZ5/NGQDqe7A2s4opv5s.S7KaS8NdfBGKqjm/nxgtQeLhmCFbTMBMua6Uvz1zTEOIx0";
 
     extraGroups = [
       "wheel"
